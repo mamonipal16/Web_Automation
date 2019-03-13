2c9c8c9577dfb4370737c4044d8c107274654398
mpal <mpal@QAAUTOSRV10.corp.wan>
2019-03-13 05:32:38 -0500

    2st commit
    Change-Id: Iee1b81e82756b6367adf4c957bc50ad06df34d39
    Signed-off-by: stripathi <stripathi@QAAUTOSRV8.corp.wan>

---
+++ src/main/java/com/csipre/web/pageobjects/mamoni.java
@@ -0,0 +1,10 @@
+package com.csipre.web.pageobjects;
+
+public class mamoni {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("final");
+	}
+
+}
